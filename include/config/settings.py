c1a8d5fcbbe2b989a8f19fb8859390fb683924f0
igor tiburcio <igor.maciel@ufrpe.br>
2026-08-10 20:50:05 -0300

    feat: atualiza configuracoes de ambiente, foi adicionado minIO

--- include/config/settings.py
+++ include/config/settings.py
@@ -2,31 +2,23 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv()  # garante que .env é lido ao executar scripts localmente
+load_dotenv()
 
-
-# ─── Caminhos ─────────────────────────────────────────────────────────────────
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
 
 # Diretórios de Dados
 DATA_DIR   = PROJECT_ROOT / "data"
-RAW_DIR    = DATA_DIR / "raw"
-CEMADEN_DIR = RAW_DIR / "api_cemaden"
 
-# Caminho do Banco de Dados
+# Banco de Dados DuckDB
 DB_PATH = str(DATA_DIR / "pepluvi.duckdb")
 
-# URLs e Configurações de Scraping
+# URLs Scraping APAC
 BASE_URL = os.getenv("APAC_BASE_URL", "http://old.apac.pe.gov.br/meteorologia/monitoramento-pluvio.php")
-
-# Certifique-se de que os diretórios existem
-RAW_DIR.mkdir(parents=True, exist_ok=True)
 
 # URL API IBGE
 URL_API_IBGE = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/26/municipios"
 
-
-# ─── MinIO (Object Storage) ───────────────────────────────────────────────────
+# MinIO 
 MINIO_ENDPOINT   = os.getenv("MINIO_ENDPOINT", "http://localhost:9000")
 MINIO_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY_ID", "")
 MINIO_SECRET_KEY = os.getenv("AWS_SECRET_ACCESS_KEY", "")
@@ -39,3 +31,4 @@ BUCKETS = {
     "tomorrow":      "tomorrow-api",
 }
 
+DATA_DIR.mkdir(parents=True, exist_ok=True)
